511e8300204cd0eb6aa8128fba85073feec02c45
Perul Jain <perul365@gmail.com>
2021-02-06 04:34:45 +0530

    'patch'

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ async def read_meme(meme_id: int, body : Body):
     result = await database.execute(query)
     if(result==0): 
         raise HTTPException(status_code=404, detail="Item not found")
-    return {"message" : "updated successfully"}
+    return result
 
 @app.delete("/memes/{meme_id}",  status_code = status.HTTP_200_OK)
 async def read_meme(meme_id: int):
